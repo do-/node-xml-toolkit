f2f4f34ca06166c19b6af20457acf9009609b4df
Dmitry Ovsyanko <do_@pochta.ru>
2021-11-16 21:27:23 +0300

    XMLLexer: storing position

--- test/test.js
+++ test/test.js
@@ -25,11 +25,39 @@ console.log (xml)
 
 }
 
+async function test_002_lexer_stream (fn) {
+
+	const is = fs.createReadStream ('test/' + fn, {
+	})
+	
+	const lexer = new XMLLexer ({
+//		maxLength: 40,
+//		encoding: 'ascii',
+	})
+
+	lexer.on ('data', data => console.log ({data}))
+	
+	return new Promise ((ok, fail) => {
+		
+		is.on ('error', fail)
+		lexer.on ('error', fail)
+		
+		lexer.on ('end', ok)
+		
+		is.pipe (lexer)
+		
+	})
+
+}
+
 async function main () {
 
-	await test_001_lexer_sync ('E05a.xml')
+//	await test_001_lexer_sync ('E05a.xml')
 //	await test_001_lexer_sync ('not-sa01.xml')
 //	await test_001_lexer_sync ('not-sa02.xml')
+//	await test_001_lexer_sync ('param_types.xml')
+//	await test_001_lexer_sync ('E05a.xml')
+	await test_002_lexer_stream ('param_types.xml')
 
 }
 
