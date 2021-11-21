b3d3215403064fc36e133cae8de5a848d0586956
Dmitry Ovsyanko <do_@pochta.ru>
2021-11-21 14:44:33 +0300

    SAXEventEmitter remade

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Attributes      = require ('./lib/Attributes')
 const XMLLexer        = require ('./lib/XMLLexer')
 const SAXEventEmitter = require ('./lib/SAXEventEmitter')
+const SAXEvent        = require ('./lib/SAXEvent')
 
-module.exports = {SAXEventEmitter, XMLLexer, Attributes}
+module.exports = {SAXEventEmitter, XMLLexer, Attributes, SAXEvent}
