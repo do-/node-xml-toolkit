30b7b63b31c0e5c38824acf6d9b579143c7dc465
Dmitry Ovsyanko <do_@pochta.ru>
2022-12-16 14:59:02 +0300

    XMLMarshaller: text + attributes

--- test/test.js
+++ test/test.js
@@ -394,8 +394,8 @@ async function test_009_schemata (fn) {
 
 	const data = {
 		AckRequest: {
-			AckTargetMessage: {Id: "U${rp_message_id}", accepted: true},
-//			AckTargetMessage: 11111,
+//			AckTargetMessage: {Id: 1, accepted: true, null: 2},
+			AckTargetMessage: 11111,
 		}
 	}
 
@@ -497,11 +497,11 @@ async function main () {
 // 	await test_003_emitter_sync ('ent.xml')
 //	await test_003_emitter_sync ('soap.xml')
 
-//	await test_004_schemata ()
-//	await test_005_schemata ()
-//	await test_006_schemata ()
-//	test_007_wsdl ()
-//	await test_008_schemata ()
+	await test_004_schemata ()
+	await test_005_schemata ()
+	await test_006_schemata ()
+	test_007_wsdl ()
+	await test_008_schemata ()
 	
 	test_009_schemata ('smev-message-exchange-service-1.1.xsd')
 //	test_009_schemata ('sign.xsd')
