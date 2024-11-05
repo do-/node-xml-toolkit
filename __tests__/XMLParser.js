b21e8a7a59a451f67085c8375b998fa5792a5d0c
Dmitry Ovsyanko <do_@pochta.ru>
2024-11-05 17:08:37 +0300

    NamespacesMap test coverage

--- __tests__/XMLParser.js
+++ __tests__/XMLParser.js
@@ -9,9 +9,21 @@ test ('basic', () => {
 	expect (XMLNode.toObject () (d)).toStrictEqual ({
 		count: '1',
 		uniqueCount: '1',
+		xmlnsxmlns: "ok",
 		si: {
 			t: 'MASS PROPERTIES for POST&DSENDS, NOMINAL and DISPERSED'
 		},
 	})
+
+	{
+
+		const {_nsMap} = d.attributes
+
+		expect (_nsMap.getNamespaceURI ('0:0')).toBeNull ()
+
+		expect ([..._nsMap.getQNames ('id', 'http://www.w3.org/XML/1998/namespace')]).toStrictEqual (['xml:id'])
+		expect ([..._nsMap.getQNames ('id', 'http://schemas.openxmlformats.org/spreadsheetml/2006/main')]).toStrictEqual (['id'])
+
+	}
 	
 })
