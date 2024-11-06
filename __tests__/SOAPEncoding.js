ffaad0ef9faa1a150d0541ba303335ad53174431
Dmitry Ovsyanko <do_@pochta.ru>
2024-11-06 20:35:11 +0300

    SOAPEncoding test coverage

--- __tests__/SOAPEncoding.js
+++ __tests__/SOAPEncoding.js
@@ -1,6 +1,15 @@
 const fs = require ('fs')
 const {XMLParser, SOAPEncoding} = require ('../')
 
+test ('bad', () => {
+
+	expect (() => new SOAPEncoding ().decode (new XMLParser ().process ('<a xsi:type="xml:id" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" />'))).toThrow ('how to')
+	expect (() => new SOAPEncoding ().decode (new XMLParser ().process ('<a xsii:type="xml:id" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" />'))).toThrow ('xsi:type')
+	expect (() => new SOAPEncoding ().decode (new XMLParser ().process ('<a xsi:type="SOAP-ENC:id" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" />'))).toThrow ('how to')
+	expect (() => new SOAPEncoding ().decode (new XMLParser ().process ('<a xsi:type="ns2:id" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ns2="http://xml.apache.org/xml-soap" />'))).toThrow ('how to')
+ 
+})
+
 test ('basic', () => {
 
 	const p = new XMLParser ()
