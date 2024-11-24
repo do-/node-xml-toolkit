4beae466af59b3d76ce9f5c363e24e5fb2426e0d
Dmitry Ovsyanko <do_@pochta.ru>
2024-11-24 11:59:44 +0300

    SOAP11Fault test FIX (validity)

--- __tests__/SOAP11Fault.js
+++ __tests__/SOAP11Fault.js
@@ -1,25 +1,37 @@
+const Path = require ('path')
+const {execSync} = require ('child_process')
 const {SOAP11, SOAPFault} = require ('../')
+
+const xsdPath = Path.join (__dirname, '..', 'lib', 'soap-1.1.xsd')
+const validate = input => execSync (`xmllint --schema ${xsdPath} -`, {input, stdio: 'pipe'})
 
 test ('basic', () => {
 
-	const fault = new SOAPFault ('Test Message', {detail: 111})
+	const fault = new SOAPFault ('Test Message', {detail: '<id>1</id>'})
 
 	const err = SOAP11.createError (fault)
 
 	expect (err.statusCode).toBe (500)
 	expect (err.message).toMatch (':Server</faultcode>')
 	expect (err.message).toMatch ('>Test Message<')
-	expect (err.message).toMatch ('detail>111</')
+	expect (err.message).toMatch ('detail><id>1</id></')
 	expect (err.headers).toStrictEqual ({'Content-Type': 'text/xml'})
+
+	expect (err.expose).toBe (true)
+	expect (err.headers).toStrictEqual ({'Content-Type': 'text/xml'})
+
+	validate (err.message)
 
 })
 
-test ('basic', () => {
+test ('code', () => {
 
-	const fault = new SOAPFault ('Test Message', {detail: 111, code: {localName: 'Fate', namespaceURI: 'http://schemas.xmlsoap.org/soap/envelope/'}})
+	const fault = new SOAPFault ('Test Message', {code: {localName: 'Fate', namespaceURI: 'http://schemas.xmlsoap.org/soap/envelope/'}})
 
 	const err = SOAP11.createError (fault)
 
 	expect (err.message).toMatch (':Fate</faultcode>')
+
+	validate (err.message)
 	
 })
