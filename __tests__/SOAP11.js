115115bd373400deb3e195c460af21d3592286b2
Dmitry Ovsyanko <do_@pochta.ru>
2025-03-20 12:07:07 +0300

    SOAP11: +guessing SOAPAction from `portType` (when no `binding` defined)

--- __tests__/SOAP11.js
+++ __tests__/SOAP11.js
@@ -35,3 +35,42 @@ test ('legacy', async () => {
 	})
 
 })
+
+test ('no binding', async () => {
+
+	const soap11 = await SOAP11.fromFile ('__data__/20184.wsdl')
+
+	const person  = {"LastName":"ИВАНОВА","FirstName":"ПЕТР","SecondName":null,"BirthDate":"1970-11-11"}
+	const address = {"Region":{"Code":"78","Name":"Санкт-Петербург"},"Street":{"Code":6597,"Name":"Московский пр-кт"},"House":"д. 18 литера Е","Flat":"33"}
+
+	{
+	
+		const d = {GetForm7Async:{person, address}}
+
+		const s = soap11.http ([d])
+
+		expect (s.method).toBe ('POST')
+
+		expect (s.headers).toStrictEqual ({
+			'Content-Type': 'text/xml; charset=utf-8',
+			SOAPAction: 'http://tempuri.org/IF7Service/GetForm7Async'
+		})
+
+	}
+
+	{
+	
+		const d = {GetForm7AsyncResponse:{person, address}}
+
+		const s = soap11.http ([d])
+
+		expect (s.method).toBe ('POST')
+
+		expect (s.headers).toStrictEqual ({
+			'Content-Type': 'text/xml; charset=utf-8',
+			SOAPAction: ''
+		})
+
+	}
+
+})
