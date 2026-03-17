de253e795e9c051193c7f90d545ab22edcf2a5ba
Dmitry Ovsyanko <dovsyanko@cheby.ru>
2026-03-17 21:28:58 +0300

    XMLSchemata: +include WIP

--- __tests__/XMLSchemata.js
+++ __tests__/XMLSchemata.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const { XMLSchemata } = require('../')
+const { XMLSchemata, XMLParser, XMLNode} = require ('../')
 
 test('cyclic import', async () => {
     const xsdPath = path.join(__dirname, '..', '__data__', '30213','RequestEGRN_v026.xsd')
@@ -17,5 +17,33 @@ test('cyclic import', async () => {
     expect (schemata.getDebugQName ('order', null)).toBe ('order')
     expect (schemata.getDebugQName ('order', undefined)).toBe ('order')
     expect (schemata.getDebugQName ('order', 'http://tempuri.org/')).toBe ('{http://tempuri.org/}order')
+
+})
+
+test.only ('include', () => {
+
+    const xs = new XMLSchemata (path.join (__dirname, '..', '__data__', 'xsinc', 'app.xsd'))
+
+    const xml = `
+        <gml:FeatureCollection xmlns:gml=\"http://www.opengis.net/gml/3.2\" xmlns:app=\"http://app\">
+            <gml:featureMember>
+                <app:ElectricLine><app:GLOBALID>{test-uuid}</app:GLOBALID></app:ElectricLine>
+            </gml:featureMember>
+        </gml:FeatureCollection>
+    `.trim ()
+
+    const d = new XMLParser ({
+        xs
+    }).process (xml)
+
+    const o = XMLNode.toObject () (d)
+
+    expect (o).toEqual ({
+        featureMember: { 
+            ElectricLine: { 
+                GLOBALID: '{test-uuid}' 
+            } 
+        } 
+    })
 
 })
