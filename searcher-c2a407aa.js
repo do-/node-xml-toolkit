cf63c9a294cc418d3366d95b432c812fb6474b93
do- <do-@users.noreply.github.com>
2026-05-02 12:54:06 +0000

    deploy: 0f1f2fd675530fc4570d3241bc1c470fde7ff6f7

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b17cc8a7.js',
+                path_to_root + 'searchindex-851169c3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
